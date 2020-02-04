2181c67a977d29c9af77e639f0f9352e64d7186f
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-04 03:10:46 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,19 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-    @post = Post.find(params[:id])
-    render :show
   end
 
   def edit
-    if @post.valid?
-      @post.update(post_params)
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
   end
 
 def update
-  @post = Post.find(params[:id])
-  if
+  @post.update(post_params)
+  if @post.valid?
+    redirect_to post_path(@post)
+  else
+    render :edit
+  end
 end
-
   private
 
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title
-  validates :category, inclusion: { in: %w("Fiction", "Non-Fiction")}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validates :content, length: { minimum: 100}
 end
